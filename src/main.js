f9744d4a46050c24884c6e25a9c2d9c8fe358582
Moisés Ferreira <moisesaoaoao@gmail.com>
2025-12-23 11:55:27 -0300

    Avanços na animação de vitória

--- src/main.js
+++ src/main.js
@@ -6,16 +6,16 @@ let victoryIndices = [];
 
 const victoryWays = [
   // Horizontais
-  { combo: [0, 1, 2], style: "top: 15.6%; left:-5%; width: 110%; rotate: 0deg;" },
-  { combo: [3, 4, 5], style: "top: 49%; left:-5%; width: 110%; rotate: 0deg;" },
-  { combo: [6, 7, 8], style: "top: 82.3%; left:-5%; width: 110%; rotate: 0deg;" },
+  { combo: [0, 1, 2], style: "top: 15.1%; left:-5%; width: 110%; rotate: 0deg;" },
+  { combo: [3, 4, 5], style: "top: 48.5%; left:-5%; width: 110%; rotate: 0deg;" },
+  { combo: [6, 7, 8], style: "top: 81.8%; left:-5%; width: 110%; rotate: 0deg;" },
   // Verticais
   { combo: [0, 3, 6], style: "top: -5%; left:16.6%; width: 110%; rotate: 90deg;" },
   { combo: [1, 4, 7], style: "top: -5%; left:50%; width: 110%; rotate: 90deg;" },
   { combo: [2, 5, 8], style: "top: -5%; left: 83.5%; width: 110%; rotate: 90deg;" },
   // Diagonais
-  { combo: [0, 4, 8], style: "top: -1%; left: 0%; width: 142%; rotate: 45deg;" },
-  { combo: [2, 4, 6], style: "top: 5%; left: 95%; width: 125%; rotate: 135deg;" },
+  { combo: [0, 4, 8], style: "top: -1.5%; left: 0%; width: 142%; rotate: 45deg;" },
+  { combo: [2, 4, 6], style: "top: -1.5%; left: 100%; width: 142%; rotate: 135deg;" },
 ];
 
 function checkVictory() {
@@ -40,12 +40,12 @@ function checkVictory() {
   // Verificando vitórias nas diagonais
   if(state[4] === "") return false;
 
-  if(state[0] === state[4] === state[8]) {
+  if(state[0] === state[4] && state[4] === state[8]) {
     victoryIndices.push(0, 4, 8);
     return true;
   }
 
-  if(state[2] === state[4] === state[6]) {
+  if(state[2] === state[4] && state[4] === state[6]) {
     victoryIndices.push(2, 4, 6);
     return true;
   }
@@ -54,7 +54,42 @@ function checkVictory() {
 }
 
 function win() {
+  let colorLine;
+  if(playerX) colorLine = "bg-red-500";
+  else colorLine = "bg-blue-500";
 
+  const victoryLine = document.createElement("div");
+  victoryLine.classList.add("absolute", colorLine, "h-[3%]", "rounded-full", "z-10", "origin-left", "animate-victory-line");
+  
+  for(const item of victoryWays) {
+    if(item.combo.length === victoryIndices.length && item.combo.every((value, index) => value === victoryIndices[index])) {
+      victoryLine.style.cssText = item.style;
+    }
+  }
+
+  const i0 = document.querySelector(`#s${victoryIndices[0]} svg`);
+  const i1 = document.querySelector(`#s${victoryIndices[1]} svg`);
+  const i2 = document.querySelector(`#s${victoryIndices[2]} svg`);
+
+  board.classList.add("pointer-events-none");
+
+  setTimeout(() => {
+    board.appendChild(victoryLine);
+  }, 500);
+
+  setTimeout(() => {
+      victoryLine.classList.remove("animate-victory-line");
+      i0.classList.add("animate-pop-in-victory");
+      i1.classList.add("animate-pop-in-victory");
+      i2.classList.add("animate-pop-in-victory");
+  }, 1500);
+
+  setTimeout(() => {
+    board.classList.remove("pointer-events-none");
+    i0.classList.remove("animate-pop-in-victory");
+    i1.classList.remove("animate-pop-in-victory");
+    i2.classList.remove("animate-pop-in-victory", "animate-pop-in");
+  }, 3000);
 }
 
 board.addEventListener('click', event => {
@@ -101,7 +136,4 @@ board.addEventListener('click', event => {
   moves++;
   playerX = !playerX;
 
-  const victoryLine = document.getElementById("victory-line");
-  victoryLine.classList.add("animate-victory-line");
-  victoryLine.classList.remove("hidden");
 });
